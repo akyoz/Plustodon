a1cf8d49a430213c0040027b6bd90613fa915012
akyoz <akyoz@akyoz.jp>
2022-11-12 23:00:02 +0900

    3.5.3+

--- app/javascript/mastodon/actions/search.js
+++ app/javascript/mastodon/actions/search.js
@@ -14,6 +14,8 @@ export const SEARCH_EXPAND_REQUEST = 'SEARCH_EXPAND_REQUEST';
 export const SEARCH_EXPAND_SUCCESS = 'SEARCH_EXPAND_SUCCESS';
 export const SEARCH_EXPAND_FAIL    = 'SEARCH_EXPAND_FAIL';
 
+export const RESULTS_LIMIT = 20;
+
 export function changeSearch(value) {
   return {
     type: SEARCH_CHANGE,
@@ -42,7 +44,7 @@ export function submitSearch() {
       params: {
         q: value,
         resolve: true,
-        limit: 5,
+        limit: RESULTS_LIMIT,
       },
     }).then(response => {
       if (response.data.accounts) {
