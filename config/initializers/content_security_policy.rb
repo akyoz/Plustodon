0d40be1377f87d308ad49fe99784f2b95ff42271
akyoz <akyoz@akyoz.jp>
2024-02-27 11:16:34 +0900

    fix csp for instanceticker

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -46,9 +46,9 @@ Rails.application.config.content_security_policy do |p|
   p.default_src     :none
   p.frame_ancestors :none
   p.font_src        :self, assets_host
-  p.img_src         :self, :https, :data, :blob, assets_host
-  p.style_src       :self, assets_host
-  p.media_src       :self, :https, :data, assets_host
+  p.img_src         :self, :data, :blob, *media_hosts, 'https://34.si/402/'
+  p.style_src       :self, assets_host, 'https://34.si/402/'
+  p.media_src       :self, :data, *media_hosts
   p.frame_src       :self, :https
   p.manifest_src    :self, assets_host
 
@@ -65,11 +65,11 @@ Rails.application.config.content_security_policy do |p|
     webpacker_public_host = ENV.fetch('WEBPACKER_DEV_SERVER_PUBLIC', Webpacker.config.dev_server[:public])
     webpacker_urls = %w(ws http).map { |protocol| "#{protocol}#{Webpacker.dev_server.https? ? 's' : ''}://#{webpacker_public_host}" }
 
-    p.connect_src :self, :data, :blob, assets_host, media_host, Rails.configuration.x.streaming_api_base_url, *webpacker_urls
-    p.script_src  :self, :unsafe_inline, :unsafe_eval, assets_host
+    p.connect_src :self, :data, :blob, *media_hosts, Rails.configuration.x.streaming_api_base_url, *front_end_build_urls
+    p.script_src  :self, :unsafe_inline, :unsafe_eval, assets_host, 'https://static.cloudflareinsights.com/', 'https://stats.g.doubleclick.net'
   else
-    p.connect_src :self, :data, :blob, assets_host, media_host, Rails.configuration.x.streaming_api_base_url
-    p.script_src  :self, assets_host, "'wasm-unsafe-eval'"
+    p.connect_src :self, :data, :blob, *media_hosts, Rails.configuration.x.streaming_api_base_url
+    p.script_src  :self, assets_host, "'wasm-unsafe-eval'", 'https://static.cloudflareinsights.com/', 'https://stats.g.doubleclick.net'
   end
 end
 
@@ -84,7 +84,7 @@ Rails.application.config.content_security_policy_nonce_directives = %w(style-src
 
 Rails.application.reloader.to_prepare do
   PgHero::HomeController.content_security_policy do |p|
-    p.script_src :self, :unsafe_inline, assets_host
+    p.script_src :self, :unsafe_inline, assets_host, 'https://static.cloudflareinsights.com/', 'https://stats.g.doubleclick.net'
     p.style_src  :self, :unsafe_inline, assets_host
   end
 
@@ -98,8 +98,8 @@ Rails.application.reloader.to_prepare do
       p.connect_src     :none
       p.frame_ancestors :self
       p.frame_src       :self
-      p.script_src      :unsafe_inline
-      p.style_src       :unsafe_inline
+      p.script_src      :unsafe_inline, 'https://static.cloudflareinsights.com/', 'https://stats.g.doubleclick.net'
+      p.style_src       :unsafe_inline, 'https://34.si/402/'
       p.worker_src      :none
     end
 
